51fc03e58fd0651ec3718a6631e9f819ce98635d
KelFoodApp <33799239+KelFoodApp@users.noreply.github.com>
2017-11-19 15:22:38 +0800

    v1.0

---
+++ js/firebase.js
@@ -0,0 +1,14 @@
+ // Initialize Firebase
+
+  var config = {
+    apiKey: "AIzaSyDbgVvCntRaFuAMSCd_wmI33AurwFRHCXU",
+    authDomain: "fooddb-6c9e6.firebaseapp.com",
+    databaseURL: "https://fooddb-6c9e6.firebaseio.com",
+    projectId: "fooddb-6c9e6",
+    storageBucket: "fooddb-6c9e6.appspot.com",
+    messagingSenderId: "693289623273"
+  };
+  firebase.initializeApp(config);
+
+
+ 
